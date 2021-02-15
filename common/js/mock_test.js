ab9337339d9a6ea009636cc774de79d375ca26cc
shichengbang <scb971063110@gmail.com>
2021-02-15 16:27:56 +0900

    任務三

---
+++ common/js/mock_test.js
@@ -0,0 +1,10 @@
+const list = [
+	{
+		url:'/api/login',
+		data:{
+			status:100,
+			message:'登録成功しました'
+		}
+	}
+]
+export default list
--- main.js
+++ main.js
@@ -6,6 +6,16 @@ import VueI18n from 'vue-i18n'
 import { getI18nMessage, getDefaultLocale } from '@/common/js/i18n.js'
 import { getLang } from '@/common/js/storage.js'
 
+//任務参
+import mockjs from 'mockjs'
+import mockTest from '@/common/js/mock_test.js'
+console.log(mockjs)
+console.log(mockTest)
+
+for(let i=0;i<mockTest.length;i++){
+	mockjs.mock(mockTest[i].url,mockTest[i].data)
+}
+
 Vue.use(VueI18n)  
 
 // 使用vuex的简单store模式 https://ask.dcloud.net.cn/article/35021
